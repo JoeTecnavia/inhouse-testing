098555c4ca0562a41cda428363d0afd50f514e1a
JoeTecnavia <41215160+JoeTecnavia@users.noreply.github.com>
2018-08-23 08:48:33 -0500

    Create testpage.js

---
+++ test/page/testpage.js
@@ -0,0 +1,26 @@
+module.exports = {
+    url : 'http://englewoodsun.fl.newsmemory.com/?pSetup=englewoodsun_ta',
+        elements: {
+        currentDate: {
+            //selector: '//*[@id="titles"]/div[3]'
+            selector: '#pullDownDate > option:nth-child(1)'
+          },
+          acceptButton: {
+            selector: '#dlg_btn_ok'
+          }
+    },
+
+    commands: [{
+        getCurrDay: function(){
+            return this.elements.currentDate.selector;
+        },
+
+        formatMonth: function(number){
+            let result = number+1;
+            if(result<10){
+                result = "0"+result;
+            }
+            return result;
+        }
+    }]
+};
